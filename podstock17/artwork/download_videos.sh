ac906aee778e0227898423b85cfa878a76a9512e
Udo Sauer <fernsehmuell@googlemail.com>
2017-07-30 14:54:20 +0200

    added download script for the three videos

--- podstock17/artwork/download_videos.sh
+++ podstock17/artwork/download_videos.sh
@@ -1,2 +1,3 @@
-wget www.fernsehmuell.de/podstock17/pause.mp4
-wget www.fernsehmuell.de/podstock17/intro_BG.mov
+wget -c www.fernsehmuell.de/podstock17/pause.mp4
+wget -c www.fernsehmuell.de/podstock17/outro.mp4
+wget -c www.fernsehmuell.de/podstock17/intro_BG.mov
